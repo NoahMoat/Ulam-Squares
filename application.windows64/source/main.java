73e6f22c267779590a91e1123ef1b4ed1ad44a8e
account 1 <nhmoat@students.ntc.edu>
2021-02-05 17:19:40 -0600

    added more visualizations using arrow keys

--- application.windows64/source/main.java
+++ application.windows64/source/main.java
@@ -29,10 +29,12 @@ private int mouse_init_y;
 private int offset_init_x;
 private int offset_init_y;
 
+public int TYPE = 2;
+
 
 public void setup() {
   
- points = new Points(0,20000);
+ points = new Points(0,30000);
  gens = new Generations(); 
  gens.CreateSquares(points);
 }
@@ -40,30 +42,89 @@ public void setup() {
 public void draw() {
   background(0);
   fill(255,191,191);
+  
   for(int i = points.start; i<points.size;i++) {
-     int[] cords = points.GetValueAt(i);
-     point((cords[0]*=multiplyer)+offset_x,(cords[1]*=multiplyer)+offset_y);
-     
-    if (i*i < points.size) {
     stroke(255);
-    line(
-     (cords[0]+offset_x),
-     (cords[1]+offset_y),
-     (points.GetValueAt(i*i)[0]*multiplyer+offset_x),
-     (points.GetValueAt(i*i)[1]*multiplyer+offset_y)
-    );
+     int[] cords = points.GetValueAt(i);
+     if(multiplyer > 5 && TYPE!=1) {
+     point((cords[0]*=multiplyer)+offset_x,(cords[1]*=multiplyer)+offset_y);
+     } else {
+      cords[0]*=multiplyer;
+      cords[1]*=multiplyer;
+     }
+     
+  stroke(255, 255, 255, 50);
+    switch(TYPE) {
+      case 0:
+        if (i*i < points.size) {stroke(255);line((cords[0]+offset_x),(cords[1]+offset_y),(points.GetValueAt(i*i)[0]*multiplyer+offset_x),(points.GetValueAt(i*i)[1]*multiplyer+offset_y));}
+      break;
+      case 1:
+    float t = sqrt(i);  
+    int t_1 = floor(t);
+
+    float t_2 = t-t_1;
+    int[] c1 = points.GetValueAt(t_1);
+    int[] c2 = points.GetValueAt(t_1+1);
+    if (t_2 == 0) {
+
+      //print(c1[0]+":"+c1[1]+ " ");
+      line(cords[0]+offset_x,cords[1]+offset_y,c1[0]*multiplyer+offset_x,c1[1]*multiplyer+offset_y);
+    } else 
+    if (c1[0] != c2[0]) {
+      //print(c1[0] + ":" +c2[0] + " ");
+      int dir = c2[0] - c1[0];
+      t_2 *= dir;
+      line(cords[0]+offset_x,cords[1]+offset_y,(t_2+c1[0])*multiplyer+offset_x,c1[1]*multiplyer+offset_y);
+      
+    } else 
+    if (c1[1] != c2[1]) {
+      int dir = c2[1] - c1[1];
+      t_2 *= dir;
+      line(cords[0]+offset_x,cords[1]+offset_y,c1[0]*multiplyer+offset_x,(t_2+c1[1])*multiplyer+offset_y);
     }
+    break;
+    case 2:
+    if(i>1 && isPrimeNumber(i)) {
+      fill(255,191,191);
+      circle(cords[0]+offset_x,cords[1]+offset_y,multiplyer/1.5f);
+      if (multiplyer > 10) {
+      textSize(multiplyer/1.5f);
+      fill(0,102,153);
+      text(i,cords[0]+offset_x-multiplyer/2,cords[1]+offset_y);
+      }
+      
     }
+    break;
+    }
+      
+  }
   }
 
 
 public boolean isPrimeNumber(int number) {
- for(int i=0; i <sqrt(number) ; i++){
-    if(number%i ==0){
-       return false;
-    }
+ boolean flag = true;
+ for (int i=2; i <=number /2; ++i) {
+  if (number%i == 0) {
+   flag = false;
+   break;
+  }
  }
- return true;
+ return flag;
+}
+
+public void keyPressed() {
+ if (keyCode == LEFT) {
+   points = new Points(0,30000);
+   TYPE=2;
+ }
+  if (keyCode == RIGHT) {
+   points = new Points(0,30000);
+   TYPE=0;
+ }
+   if (keyCode == UP) {
+   points = new Points(0,2500);
+   TYPE=1;
+ }
 }
 
 
